3e8857c2f0fb040f710b5365b221ede851e9e640
julsiam <julsestorco031602@gmail.com>
2022-09-13 18:50:51 +0800

    php exercise

---
+++ index.php
@@ -0,0 +1,47 @@
+// function sampleFunction($num1, $num2){
+// if ($num1 == $num2){
+// return ($num1 + $num2)*3;
+// }
+// return ($num1 + $num2);
+// }
+// echo sampleFunction(2,4);
+
+// function sampleFunction($num1, $num2){
+// return($num1 == 30) || ($num2);
+// }
+
+// $color = array("white", "green", "red","blue", "black");
+// echo "I have $color[1] bag and a $color[0] shirt";
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <table width="270px" cellspacing="0px" cellpadding="0px" border="1px">
+        <!-- cell 270px wide (8 columns x 60px) -->
+        <?php
+        for ($row = 1; $row <= 8; $row++) {
+            echo "<tr>";
+            for ($col = 1; $col <= 8; $col++) {
+                $sum = $row + $col;
+                
+                if ($sum % 2 == 0) {
+                    echo "<td height=30px width=30px bgcolor=#FFFFFF></td>";
+                } else {
+                    echo "<td height=30px width=30px bgcolor=#000000></td>";
+                }
+            }
+            echo "</tr>";
+        }
+        ?>
+    </table>
+
+</body>
+
+</html>
